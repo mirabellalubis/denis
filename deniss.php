837bba0f600fa2aa86a074ea41b34edfba8fb7dd
deniirawan02 <61085171+deniirawan02@users.noreply.github.com>
2020-02-18 21:26:09 +0700

    Update deniss.php

--- deniss.php
+++ deniss.php
@@ -108,38 +108,39 @@ if ($register == false)
             }
             else{
                 echo "\e[300m[+] ".$claim."\n";
-                
-        }
-        tidur ( 3 );
-        $ goride1 = permintaan ( '/ promosi-promosi / v1 / promosi / pendaftaran' , $ token , '{"promo_code": "COBAINGOJEK"}' );
-        $ Message2 = fetch_value ( $ goride1 , ' "message": "' , '"' );
-        gema  "\ n" . color ( "green" , "+] Pesan:" . $ message2 );
-        tidur ( 3 );
-        $ cekvoucher = permintaan ( '/ gopoints / v3 / wallet / voucher? limit = 10 & halaman = 1' , $ token );
-        $ total = fetch_value ( $ cekvoucher , '"total_vouchers":' , ',' );
-        $ voucher3 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "3" );
-        $ voucher1 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "1" );
-        $ voucher2 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "2" );
-        $ voucher4 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "4" );
-        $ voucher5 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "5" );
-        $ voucher6 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "6" );
-        $ voucher7 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "7" );
-        gema  "\ n" . color ( "yellow" , "!] Total voucher" . $ total . ":" );
-         warna gema ( "hijau" , "1." . $ voucher1 );
-        gema  "\ n" . warna ( "hijau" , "2." . $ voucher2 );
-        gema  "\ n" . warna ( "hijau" , "3." . $ voucher3 );
-        gema  "\ n" . warna ( "hijau" , "4." . $ voucher4 );
-        gema  "\ n" . warna ( "hijau" , "5." . $ voucher5 );
-        gema  "\ n" . warna ( "hijau" , "6." . $ voucher6 );
-        gema  "\ n" . warna ( "hijau" , "7." . $ voucher7 );
-        gema "\ n" ;
-        $ expired1 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '1' );
-        $ expired2 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '2' );
-        $ expired3 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '3' );
-        $ expired4 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '4' );
-        $ expired5 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '5' );
-        $ expired6 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '6' );
-        $ expired7 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '7' );
+            sleep(3);
+            goto next8;  
+            }
+            next8 ( 3 );
+            $ goride1 = permintaan ( '/ promosi-promosi / v1 / promosi / pendaftaran' , $ token , '{"promo_code": "COBAINGOJEK"}' );
+            $ Message2 = fetch_value ( $ goride1 , ' "message": "' , '"' );
+            gema  "\ n" . color ( "green" , "+] Pesan:" . $ message2 );
+            tidur ( 3 );
+            $ cekvoucher = permintaan ( '/ gopoints / v3 / wallet / voucher? limit = 10 & halaman = 1' , $ token );
+            $ total = fetch_value ( $ cekvoucher , '"total_vouchers":' , ',' );
+            $ voucher3 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "3" );
+            $ voucher1 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "1" );
+            $ voucher2 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "2" );
+            $ voucher4 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "4" );
+            $ voucher5 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "5" );
+            $ voucher6 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "6" );
+            $ voucher7 = getStr1 ( '"title": "' , '",' , $ cekvoucher , "7" );
+            gema  "\ n" . color ( "yellow" , "!] Total voucher" . $ total . ":" );
+            warna gema ( "hijau" , "1." . $ voucher1 );
+            gema  "\ n" . warna ( "hijau" , "2." . $ voucher2 );
+            gema  "\ n" . warna ( "hijau" , "3." . $ voucher3 );
+            gema  "\ n" . warna ( "hijau" , "4." . $ voucher4 );
+            gema  "\ n" . warna ( "hijau" , "5." . $ voucher5 );
+            gema  "\ n" . warna ( "hijau" , "6." . $ voucher6 );
+            gema  "\ n" . warna ( "hijau" , "7." . $ voucher7 );
+            gema "\ n" ;
+            $ expired1 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '1' );
+            $ expired2 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '2' );
+            $ expired3 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '3' );
+            $ expired4 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '4' );
+            $ expired5 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '5' );
+            $ expired6 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '6' );
+            $ expired7 = getStr1 ( '"expiry_date": "' , '"' , $ cekvoucher , '7' );
   
 
          
